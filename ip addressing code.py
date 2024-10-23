fd7442f867e2c3c1e28d82d7840392551cb92dfb
hunter <87490244+hunterhacker29@users.noreply.github.com>
2024-10-23 22:26:17 +0530

    Update ip addressing code.py

--- ip addressing code.py
+++ ip addressing code.py
@@ -71,3 +71,82 @@ a= ip.split('.')
 
 print(f"Input ip address is : {ip}")
 clasIP(a)
+
+
+## Updated 
+def clasIP(a):
+    foctant= int(a[0])
+    
+    if foctant >=1 and foctant <=126 :
+        print (f"IP class : A")
+        mask = [255,0,0,0]
+        one =8
+        print (f"Subnet mask is {mask} or /{one}")
+        n= 32-one
+        host = 2**n 
+        print(f"Number of total host is : {host}")
+        print(f"Number of valid or usable host is : {host-2}")
+        start = []
+        end = []
+        for i in range (len(a)-1):
+            start.append(a[i])
+
+        for i in range(len(a)-3):
+            end.append(a[i])
+        
+        start.append(0)
+        end.append([255,255,255])
+        print(f"Starting address is : {'.'.join(map(str, start))}")
+        print(f"Broadcast address is : {'.'.join(map(str, end))}")
+    elif foctant >=128 and foctant <=191 :
+        print (f"IP class : B ")
+        mask = [255,255,0,0]
+        one =16
+        print (f"Subnet mask is {mask} or /{one}")
+        n= 32-one
+        host = 2**n 
+        print(f"Number of total host is : {host}")
+        print(f"Number of valid or usable host is : {host-2}")
+        start = []
+        end = []
+        for i in range (len(a)-1):
+            start.append(a[i])
+
+        for i in range(len(a)-2):
+            end.append(a[i])
+        
+        start.append(0)
+        end.append([255,255])
+        print(f"Starting address is : {'.'.join(map(str, start))}")
+        print(f"Broadcast address is : {'.'.join(map(str, end))}")
+    elif foctant >=192 and foctant <=239 :
+        print (f"IP class : C ")
+        mask = [255,255,255,0]
+        one =24
+        print (f"Subnet mask is {mask} or /{one}")
+        n= 32-one
+        host = 2**n 
+        print(f"Number of total host is : {host}")
+        print(f"Number of valid or usable host is : {host-2}")
+        start = []
+        end = []
+        for i in range (len(a)-1):
+            start.append(a[i])
+
+        for i in range(len(a)-1):
+            end.append(a[i])
+        
+        start.append(0)
+        end.append([255])
+        print(f"Starting address is : {'.'.join(map(str, start))}")
+        print(f"Broadcast address is : {'.'.join(map(str, end))}")
+        
+    else :
+        print("Invalid IP")
+
+ip = input("Enter IP address: ")
+a= ip.split('.')
+
+print(f"Input IP address is : {ip}")
+clasIP(a)
+
